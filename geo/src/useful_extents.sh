fe03ba79b3ff68649f4d25e3e1e1fcb50f40e40f
EdwardO <edoubrayrie@gmail.com>
2021-11-09 11:32:04 +0100

    slope palette: cslo, hslo, eslo ; fixes to palette & ticks ; samples ; useful extents

---
+++ geo/src/useful_extents.sh
@@ -0,0 +1,12 @@
+
+
+# also clapier.geojson"
+clapier='7.38 44.1 7.44 44.15'
+malinvern='7.163085 44.182203 7.207031 44.213709'
+paradis_z11='6.855466 45.460132 7.382813 45.583291'
+
+ign1='6.3 43.72 7.72 44.43'
+ign1_half='6.3 43.72 7.72 44.43'
+
+Aigle_Jouques_Sanremo_Zermatt='5.625 43.581 7.734 46.316'
+Lausanne_Como_Chambery='5.6250 43.5804 7.7343 46.558'
